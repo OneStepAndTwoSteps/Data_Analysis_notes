6f565d3b093eaa006c19c9fddc765933d7444081
OneStepAndTwoSteps <42194566+OneStepAndTwoSteps@users.noreply.github.com>
2019-05-20 23:02:03 +0800

    Create 决策树预测房价均方差.py

---
+++ Adaboost-KNN-决策树均方差比对/决策树预测房价均方差.py
@@ -0,0 +1,26 @@
+#author py chen
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.datasets import load_boston
+
+# 加载房价数据
+data=load_boston()
+print(data.keys())
+
+# 数据划分 训练集合测试集
+X_train,X_test,Y_train,Y_test=train_test_split(data.data,data.target,test_size=0.3)
+
+# 使用Adaboost回归模型：
+regressor=DecisionTreeRegressor()
+regressor.fit(X_train,Y_train)
+
+# 开始预测
+predicted=regressor.predict(X_test)
+mse=mean_squared_error(Y_test,predicted)
+
+print("房价预测结果：",predicted)
+# 打印均方误差，保留两位
+print("决策树均方误差为：",round(mse,2))
+
+# 决策树均方误差为：20.86
