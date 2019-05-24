9866ad33e1824e589dd09bb0d958fc2d7b8256f7
OneStepAndTwoSteps <42194566+OneStepAndTwoSteps@users.noreply.github.com>
2019-05-24 23:11:50 +0800

    Create GridSearchCV参数调优.py

---
+++ GridSearchCV模块/GridSearchCV参数调优.py
@@ -0,0 +1,18 @@
+#author py chen
+# 使用 RandomForest 对 IRIS 数据集进行分类
+# 利用 GridSearchCV 寻找最优参数
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_iris
+rf = RandomForestClassifier()
+parameters = {"n_estimators": range(1,11)}
+iris = load_iris()
+# 使用 GridSearchCV 进行参数调优
+clf = GridSearchCV(estimator=rf, param_grid=parameters)
+# 对 iris 数据集进行分类
+clf.fit(iris.data, iris.target)
+print(" 最优分数： %.4lf" %clf.best_score_)
+print(" 最优参数：", clf.best_params_)
+运行结果如下：
+最优分数： 0.9667
+最优参数： {'n_estimators': 6}
