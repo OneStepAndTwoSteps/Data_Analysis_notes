720224eeef47e4f41103cc37a81c63584cdbb775
OneStepAndTwoSteps <42194566+OneStepAndTwoSteps@users.noreply.github.com>
2019-05-26 13:53:08 +0800

    Update 使用pipeline和GridSearch进行参数调优.py

--- GridSearchCV模块/使用pipeline和GridSearch进行参数调优.py
+++ GridSearchCV模块/使用pipeline和GridSearch进行参数调优.py
@@ -1,24 +1,35 @@
 #author py chen
-# 使用 RandomForest 对 IRIS 数据集进行分类
-# 利用 GridSearchCV 寻找最优参数, 使用 Pipeline 进行流水作业
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.datasets import load_iris
-from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
-rf = RandomForestClassifier()
-parameters = {"randomforestclassifier__n_estimators": range(1,11)}
-iris = load_iris()
-pipeline = Pipeline([
-        ('scaler', StandardScaler()),
-        ('randomforestclassifier', rf)
+from sklearn.preprocessing import StandardScaler
+
+# 创建随机森林模型
+rf=RandomForestClassifier()
+
+# 加载数据
+data=load_iris()
+
+# 定义参数调优的范围,randomforestclassifier__n_estimators __前面定义的是名字，后面定义的内容是参数
+parameters={"randomforestclassifier__n_estimators":range(1,11)}
+
+# 定义pipeline 流水线
+pipeline=Pipeline([
+    ('scaler',StandardScaler()),
+    ('randomforestclassifier',rf)
 ])
-# 使用 GridSearchCV 进行参数调优
-clf = GridSearchCV(estimator=pipeline, param_grid=parameters)
-# 对 iris 数据集进行分类
-clf.fit(iris.data, iris.target)
-print(" 最优分数： %.4lf" %clf.best_score_)
-print(" 最优参数：", clf.best_params_)
-运行结果：
-最优分数： 0.9667
-最优参数： {'randomforestclassifier__n_estimators': 9}
+
+# 使用GridSearchCV 进行参数调优
+clf=GridSearchCV(estimator=pipeline,param_grid=parameters,cv=6)
+
+# 进行数据集分类
+clf.fit(data.data,data.target)
+
+# 打印最优分数 给出不同参数情况下的评价结果
+print("最优分数：%.4lf"%clf.best_score_)
+# 打印最优参数 描述了已取得最佳结果的参数的组合
+print("最优参数：%s"%clf.best_params_)
+
+# 我们在进行参数调优时，如果计算出来最优参数会发生变化，我们可以查看他的最优分数，如果最优分数相差不大，那表示我们的参数结果也相差不大，那我们选择其中一个即可
