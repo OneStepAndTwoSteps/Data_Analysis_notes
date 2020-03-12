490a726b0e59027ff2937513349fc874e259212b
victory chen <q824701419@163.com>
2020-03-12 23:38:05 +0800

    Update notes

---
+++ 爬虫项目代码/根据歌手的id对歌手的前50首热门单曲做词云展示/readme.md
@@ -0,0 +1,3 @@
+### 词云结果展示
+
+![Image_text](https://raw.githubusercontent.com/OneStepAndTwoSteps/data_mining_analysis/master/%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90%E6%A1%88%E4%BE%8B/%E6%A0%B9%E6%8D%AE%E6%AD%8C%E6%89%8B%E7%9A%84id%E5%AF%B9%E6%AD%8C%E6%89%8B%E7%9A%84%E5%89%8D50%E9%A6%96%E7%83%AD%E9%97%A8%E5%8D%95%E6%9B%B2%E5%81%9A%E8%AF%8D%E4%BA%91%E5%B1%95%E7%A4%BA/%E8%AF%8D%E4%BA%91%E7%BB%93%E6%9E%9C%E5%B1%95%E7%A4%BA.png)
---
+++ 爬虫项目代码/根据歌手的id对歌手的前50首热门单曲做词云展示/林俊杰前50首歌歌词词云展示.py
@@ -0,0 +1,108 @@
+#author py chen
+
+import requests
+import sys
+import re
+import os
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import jieba
+from PIL import Image
+import numpy as np
+from lxml import etree
+
+headers = {
+    'Referer': 'http://music.163.com',
+    'Host': 'music.163.com',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'User-Agent': 'Chrome/10'
+}
+
+
+def get_song(singer_id):
+    # 这里有一个地方要注意 我们在查看我们的歌手的时候 我们的链接是这样的https://music.163.com/#/artist?id={id} 链接里面有一个#号这里我们要注意
+    # 加了井号之后数据并不是变成了动态加载，而是我们在使用requests请求时#后面的内容不会带入到路由中也就是说我们请求的内容还是https://music.163.com
+    # 而浏览器可以进行请求是因为浏览器会把#后面的内容当成位置去找。
+    base_url="https://music.163.com/artist?id={id}"
+    singer_url=base_url.format(id=singer_id)
+    # print(singer_url)
+    # 请求页面
+    html=requests.request('GET',singer_url,headers=headers).text
+    # print(html)
+    HTML=etree.HTML(html)
+
+    # 返回前50首歌的跳转链接(其中有歌曲的id)可以通过id找歌词和歌名
+    href_xpath = "//*[@id='hotsong-list']//a/@href"
+    songs_xpath = "//*[@id='hotsong-list']//a/text()"
+    hrefs=HTML.xpath(href_xpath)
+    song_names=HTML.xpath(songs_xpath)
+    # print(hrefs)
+    # print(song_names)
+
+
+    # 记录歌曲的id和name
+    song_id_list=[]
+    song_name_list=[]
+    for href,name in zip(hrefs,song_names):
+        song_id_list.append(href.split("=")[1])
+        song_name_list.append(name)
+
+    return song_name_list,song_id_list
+
+
+def get_lyrics(song_id):
+    # 这个链接是网易云音乐歌曲的api接口
+    base_url='https://music.163.com/api/song/lyric?os=pc&id={id}&lv=-1&kv=-1&tv=-1'
+    song_url=base_url.format(id=song_id)
+
+    html=requests.request('GET',song_url,headers=headers).json()
+    print(html)
+    if 'lrc' in html:
+       lyrics=html['lrc']['lyric']
+       new_lyrics = re.sub(r'\[\S+\]', '', lyrics)
+       return new_lyrics
+    else:
+        return ''
+
+def create_wordcloud(all_lyrics):
+    print('根据词频，开始生成词云!')
+    f = remove_stop_words(all_lyrics)
+    cut_text = " ".join(jieba.cut(f, cut_all=False, HMM=True))
+    wc = WordCloud(
+        font_path="./wc.ttf",
+        max_words=100,
+        width=2000,
+        height=1200,
+    )
+    print(cut_text)
+    wordcloud = wc.generate(cut_text)
+    # 写词云图片
+    wordcloud.to_file("wordcloud.jpg")
+    # 显示词云文件
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.show()
+
+
+def remove_stop_words(all_lyrics):
+    stop_words=['作词', '作曲', '编曲', 'Arranger', '录音', '混音', '人声', 'Vocal', '弦乐', 'Keyboard', '键盘', '编辑', '助理',
+                  'Assistants', 'Mixing', 'Editing', 'Recording', '音乐', '制作', 'Producer', '发行', 'produced', 'and',
+                  'distributed']
+    for stop_word in stop_words:
+        all_lyrics = all_lyrics.replace(stop_word, '')
+    return all_lyrics
+
+if __name__ == '__main__':
+    singer_id=3684
+    song_name_list,song_id_list=get_song(singer_id)
+    print(song_name_list)
+    print(song_id_list)
+    all_lyrics=''
+    for song_name,song_id in zip(song_name_list,song_id_list):
+        lyrics=get_lyrics(song_id)
+        all_lyrics = all_lyrics+ '' +lyrics
+
+        print(song_name)
+
+    create_wordcloud(all_lyrics)
+
