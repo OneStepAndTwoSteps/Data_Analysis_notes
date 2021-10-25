f70ee13e04bca3621cb5fbbc8729b1f75e248659
victory chen <q824701419@163.com>
2021-10-25 14:07:35 +0800

    update

--- 1、数据分析相关库使用/Sklearn机器学习库/metrics/模型评估/分类评估.py
+++ 1、数据分析相关库使用/Sklearn机器学习库/metrics/模型评估/分类评估.py
@@ -6,12 +6,13 @@ y_pred= [0,1,0,0,0,1,1,1,1,1] # y_pred
 y_test= [0,0,1,0,0,1,1,1,0,1] # y_test 
 
 
-print(confusion_matrix(y_pred,y_test))
-
-# TP:3 , FP:1 , FN:2 , TN:4
-
-print(accuracy_score(y_pred,y_test))
-print(precision_score(y_pred,y_test,average='macro'))  # TP/(TP+FP)
+print(confusion_matrix(y_test,y_pred))
 
 
 
+print(accuracy_score(y_test,y_pred))
+
+print(precision_score(y_test,y_pred,average='macro'))  # TP/(TP+FP)
+print(precision_score(y_pred,y_test,average='macro'))   # 错误写法，traget 和 pred 写反了
+
+# 写反两者的计算结果是不一样的。
